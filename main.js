bb864523a18c18cafcd9cfe7afac5accbdac748c
azadar hussain <azadarhussain800@gmail.com>
2017-02-19 21:01:19 +0500

    ajax url link has been changed

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var userContainer = $('#userContainer'),
 	userName = $('#userName');
 $.ajax({
 	type : 'GET',
-	url : 'usersInfo.json',
+	url : 'https://azadar-hussain.github.io/ajax/usersInfo.json',
 	success: function(data){
 		console.log("success");
 	},
