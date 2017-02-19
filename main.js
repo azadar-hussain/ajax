44427a4c2e9e2672340984cb221c836475adf869
azadar hussain <azadarhussain800@gmail.com>
2017-02-19 21:13:21 +0500

    getting angry

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $.ajax({
 	type : 'GET',
 	url : 'https://azadar-hussain.github.io/ajax/usersInfo.json',
 	success: function(data){
-		console.log("success");
+		console.log(data);
 	},
 	error : function(){
 		console.log("error");
